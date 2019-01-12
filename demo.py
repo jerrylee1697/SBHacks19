e45e102bdc6bd5fc7ca58ccb1dc3f4e26058aa3a
Jerry <jerrylee1697@gmail.com>
2019-01-12 06:08:30 -0800

    test dynamic demo

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 from twilio.twiml.messaging_response import MessagingResponse
 
 def demo(body):
-    resp = MessagingResponse()
     if 'hello' in body:
         msg = "Hello! Thank you for contacting Hooties! How may I assist you today?"
     elif 'menu' in body:
@@ -12,4 +11,4 @@ def demo(body):
         msg = 'Todays specials are: $5 Tacos'
     elif 'happy hour' in body:
         msg = 'Happy hour is from 1pm to 3pm. The happy hour deal is: buy 1, you buy another'
-    return resp.message(msg)
+    return msg
--- main.py
+++ main.py
@@ -17,20 +17,20 @@ def sms_reply():
     # Start our TwiML response
     resp = MessagingResponse()
 
-    if 'hello' in body:
-        msg = "Hello! Thank you for contacting Hooties! How may I assist you today?"
-    elif 'menu' in body:
-        msg = "Todays menu is:\n 1. Raw Pork\n 2. Raw Chicken\n 3. Well Done A5 Wagyu"
-    elif 'hours' in body:
-        msg = "Todays hours are from 9am to 9pm."
-    elif 'specials' in body:
-        msg = 'Todays specials are: $5 Tacos'
-    elif 'happy hour' in body:
-        msg = 'Happy hour is from 1pm to 3pm. The happy hour deal is: buy 1, you buy another'
-    else:
-        msg = 'Sorry, could you type that more clearly please?'
+    # if 'hello' in body:
+    #     msg = "Hello! Thank you for contacting Hooties! How may I assist you today?"
+    # elif 'menu' in body:
+    #     msg = "Todays menu is:\n 1. Raw Pork\n 2. Raw Chicken\n 3. Well Done A5 Wagyu"
+    # elif 'hours' in body:
+    #     msg = "Todays hours are from 9am to 9pm."
+    # elif 'specials' in body:
+    #     msg = 'Todays specials are: $5 Tacos'
+    # elif 'happy hour' in body:
+    #     msg = 'Happy hour is from 1pm to 3pm. The happy hour deal is: buy 1, you buy another'
+    # else:
+    #     msg = 'Sorry, could you type that more clearly please?'
 
-    resp.message(msg)
+    resp.message(demo(body))
     # # Determine the right reply for this message
     # if 'hello' in body:
     #     resp.message("You're a ho!")
