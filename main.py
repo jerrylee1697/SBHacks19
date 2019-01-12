1f068b213cf9c50db1c5a0b83db295c7d2702188
Jerry <jerrylee1697@gmail.com>
2019-01-12 04:57:41 -0800

    Testing dynamic responses

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ app = Flask(__name__)
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
+
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
     # Start our TwiML response
     resp = MessagingResponse()
 
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("You're a ho!")
+    elif body == 'bye':
+        resp.message("Bye bitch!")
+
     # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
+    # resp.message("The Robots are coming! Head for the hills!")
 
     return str(resp)
 
